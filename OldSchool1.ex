8bacb66dfbaa2fe174510729068ab220dc2428f9
kammitama5 <krystal.maughan@gmail.com>
2016-07-29 13:57:02 -0700

    first commit

---
+++ OldSchool1.ex
@@ -0,0 +1,28 @@
+#simple old school game
+
+
+
+IO.puts("This is a simple old school game.")
+
+defmodule Name do
+
+	name = IO.gets (" What is your name?")
+		|> String.strip
+		IO.puts("Hi " <> name)
+	end
+	
+defmodule PickNum do
+	
+	num1 = IO.gets ("You are on a journey. Pick a path number: 1, 2 or 3 ----->")
+		|> String.strip
+		IO.puts("You picked Path " <> "#{num1}")
+		IO.inspect(num1)
+		
+		
+		
+		
+		end
+		
+		
+		
+		
